0a491f8be75b6be6c847318eca3f8a14d3efd6af
Ramin Joshang <rjwshng@gmail.com>
2022-06-17 19:57:33 +0430

    satisfaction section added to project, some problem in mobile device solved and added some styles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
+// Dark mode
+
+const dark = document.querySelector('.dark');
+const light = document.querySelector('.light');
+const buttons = document.querySelector('.dark-mode').children;
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', e => {
+        for (let j = 0; j < buttons.length; j++) {
+            const element = buttons[j];
+            element.classList.remove("active");
+        }
+        buttons[i].classList.add('active');
+    })
+}
+
 //  Products slider
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "1",
+    slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
     autoplay: {
